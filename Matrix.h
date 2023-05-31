bfe44baf5c3b8c89826665005ba56991626e8db0
Pepega2005 <75209223+Pepega2005@users.noreply.github.com>
2023-05-31 17:54:13 +0300

    Revert "Lab5"

--- Matrix.h
+++ Matrix.h
@@ -6,9 +6,7 @@
 
 const int MAX_COL = 4;
 
-double sumOfRow(const double mat[][MAX_COL], const int row, const int maxRow);
-double sumOfCol(const double mat[][MAX_COL], const int column, const int maxRow);
-double* sumOfCols(const double mat[][MAX_COL], const int maxRow);
-double* sumOfRows(const double mat[][MAX_COL], const int maxRow);
-void fillWithRandomNum(double mat[][MAX_COL], const int maxRow);
-void printMatrix(const double mat[][MAX_COL], const int maxRow);
+float sumOfRow(const float mat[][MAX_COL], const int row, const int maxRow);
+float sumOfCol(const float mat[][MAX_COL], const int column, const int maxRow);
+void fillWithRandomNum(float mat[][MAX_COL], const int maxRow);
+void printMatrix(const float mat[][MAX_COL], const int maxRow);
